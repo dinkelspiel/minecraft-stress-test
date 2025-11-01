6e7c31a3439187cfdce4664e84b332b6254556a8
keiidev <mail@keii.dev>
2025-11-01 16:36:43 +0100

    asd

---
+++ start.sh
@@ -0,0 +1,1 @@
+java -Dbot.offset=300 -Dbot.count=2000 -Dbot.ip=104.243.41.24 -Dbot.port=25538 -Dbot.login.delay.ms=500 -Dbot.radius=1000 -jar minecraft-stress-test-1.0.0-SNAPSHOT-jar-with-dependencies.jar
